90dfdd285ca962a897f0996093520f5f67844e36
omkar <omkarsbhatkande@gmail.com>
2024-08-06 11:58:55 +0530

    done with todo app

--- todoApp/app.js
+++ todoApp/app.js
@@ -16,5 +16,26 @@ function addTask(){
     span.innerHTML = "\u00d7";
     li.appendChild(span);
   }
-  inputBox.value
+  inputBox.value = "";
+  saveData();
 }
+
+list.addEventListener("click", (e)=>{
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    saveData();
+  }
+  else if(e.target.tagName === "SPAN"){
+    e.target.parentElement.remove();
+    saveData();
+  }
+},false);
+
+function saveData(){
+  localStorage.setItem("data" , list.innerHTML)
+}
+
+function showTask(){
+  list.innerHTML = localStorage.getItem("data");
+}
+showTask();
