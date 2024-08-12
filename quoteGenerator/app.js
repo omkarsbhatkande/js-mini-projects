fbc379bcfee688f03279ea919101215aac7a0d1a
omkar <omkarsbhatkande@gmail.com>
2024-08-12 11:17:11 +0530

    done with project

--- quoteGenerator/app.js
+++ quoteGenerator/app.js
@@ -1,10 +1,21 @@
 const apiUrl = "https://favqs.com/api/qotd";
 
+const quote = document.getElementById("quote");
+
+const author = document.getElementById("author");
+
 async function getQuote(url) {
   const response =  await fetch(url);
   let data = await response.json();
-  console.log(data);
+  //console.log(data);
+  quote.innerHTML = data.content;
+  author.innerHTML=data.author;
   
 }
 
 getQuote(apiUrl);
+
+
+function tweet() {
+  window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML +"...by" +author.innerHTML , "tweet Window" , "width=600 , height=300");
+}
