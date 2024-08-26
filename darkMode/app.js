a16ebed3595021e0a1a020333b637c2b64389dae
omkar <omkarsbhatkande@gmail.com>
2024-08-26 09:57:39 +0530

    done

---
+++ darkMode/app.js
@@ -0,0 +1,11 @@
+let icon = document.getElementById("icon");
+
+icon.onclick=()=>{
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "sun.png";
+  }
+  else{
+    icon.src = "moon.png";
+  }
+}
