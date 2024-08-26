c690f06324c63b7f900b1b6ab81745ad539b487a
omkar <omkarsbhatkande@gmail.com>
2024-08-26 14:33:42 +0530

    done

--- formValidation/app.js
+++ formValidation/app.js
@@ -14,6 +14,61 @@ function validateName(){
     nameError.innerHTML = " Write Full Name"
     return false;
   }
-  nameError.innerHTML = "valid"
+  nameError.innerHTML = "<i class='fas fa-check-circle'></I>";
   return true;
+}
+
+function validatePhone(){
+  let phone = document.getElementById("contact-phone").value;
+  if (phone.length == 0) {
+    phoneError.innerHTML = " Phone no Is Required"
+    return false;
+  }
+  if(phone.length !== 10){
+    phoneError.innerHTML = " Write valid Number"
+    return false;
+  }
+  if(!phone.match(/^[0-9]{10}$/)){
+    phoneError.innerHTML = " Write valid Number"
+    return false;
+  }
+  phoneError.innerHTML = "<i class='fas fa-check-circle'></I>";
+  return true;
+}
+
+function validateEmail(){
+  let email = document.getElementById("contact-email").value;
+  if (email.length == 0) {
+    emailError.innerHTML = " Email Is Required"
+    return false;
+  }
+  if(!email.match(/^[A-Za-z]\._\-[0-9]*[\.][a-z]{2,4}$/)){
+    emailError.innerHTML = " Write valid Email"
+    return false;
+  }
+  emailError.innerHTML = "<i class='fas fa-check-circle'></I>";
+  return true;
+}
+
+function validatemsg(){
+  let msg = document.getElementById("contact-msg").value;
+  let required = 30 ;
+  let left = required - msg.length;
+  if (left>0) {
+    msgError.innerHTML = left + " More characters"
+    return false;
+  }
+  msgError.innerHTML = "<i class='fas fa-check-circle'></I>";
+  return true;
+}
+
+function validateform(){
+  if (!validateName() || validatePhone || validateEmail || validatemsg) {
+    submitError.style.display = "block"
+    emailError.innerHTML = " Please fix Error to submit"
+    setTimeout(()=>{
+      submitError.style.display = "none"
+    },2000)
+    return false;
+  }
 }
