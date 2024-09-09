eee95b50d718ca360ffe00293f694d5f333e53ec
omkar <omkarsbhatkande@gmail.com>
2024-09-09 10:30:43 +0530

    front part in done api need to be add

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
-function analyzeArray(arr) {
-  if (arr.length === 0) return { max: null, min: null, duplicates: [] };
+// function analyzeArray(arr) {
+//   if (arr.length === 0) return { max: null, min: null, duplicates: [] };
 
-  let maxNum = Math.max(...arr);
-  let minNum = Math.min(...arr);
-  let duplicates = [...new Set(arr.filter((item, index) => arr.indexOf(item) !== index))];
+//   let maxNum = Math.max(...arr);
+//   let minNum = Math.min(...arr);
+//   let duplicates = [...new Set(arr.filter((item, index) => arr.indexOf(item) !== index))];
 
-  return { max: maxNum, min: minNum, duplicates: duplicates };
+//   return { max: maxNum, min: minNum, duplicates: duplicates };
+// }
+
+// // Example usage:
+// const arr = [2, 4, 1, 7, 2, 8, 4];
+// const result = analyzeArray(arr);
+
+// console.log(`Max number: ${result.max}`);
+// console.log(`Min number: ${result.min}`);
+// console.log(`Duplicate numbers: ${result.duplicates}`);
+
+
+
+// const num = [1,2,3,4];
+
+// const ab = num.map(num =>num*2);
+
+// console.log(ab);
+
+// const numbers = [1, 2, 3, 4];
+// const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+// console.log(sum); // 10
+
+function greet(name, callback) {
+  console.log('Hello ' + name);
+  callback();
 }
 
-// Example usage:
-const arr = [2, 4, 1, 7, 2, 8, 4];
-const result = analyzeArray(arr);
+function sayGoodbye() {
+  console.log('Goodbye');
+}
 
-console.log(`Max number: ${result.max}`);
-console.log(`Min number: ${result.min}`);
-console.log(`Duplicate numbers: ${result.duplicates}`);
+greet('John', sayGoodbye);
+
